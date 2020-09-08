28c37b42d73ea25f2f1e73e4a063f35f6dacc957
LisaRamaekers <lisa.ramaekers@student.ucll.be>
2020-09-08 15:46:07 +0200

    Outputting Text

---
+++ Outputting Text/Outputting Text/Outputting Text.cpp
@@ -0,0 +1,17 @@
+//Outputting Text
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Starting program.." << flush;
+	
+	cout << "This is some text." << endl;
+	
+	cout << "Banana. " << "Apple. " << "Orange. " << endl;
+	
+	cout << "This some more text." << endl;
+	
+	return 0;
+}
