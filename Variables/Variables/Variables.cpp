cf3b0b08a67c3b73cbd12aca42fdaf832e4cf41e
LisaRamaekers <lisa.ramaekers@student.ucll.be>
2020-09-08 15:56:52 +0200

    Variables

---
+++ Variables/Variables/Variables.cpp
@@ -0,0 +1,26 @@
+// Variables
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int numberCats = 5;
+	int numberDogs = 7;
+	int numberAnimals = numberCats + numberDogs;
+
+	cout << "Number of cats: " << numberCats << endl;
+	cout << "Number of dogs: " << numberDogs << endl;
+
+	cout << "Total animals: " << numberAnimals << endl;
+
+	cout << "New dog acquired!" << endl;
+
+	numberDogs = numberDogs + 1;
+
+	cout << "New number of dogs: " << numberDogs << endl;
+
+	cout << "Total animals: " << numberAnimals << endl;
+
+	return 0;
+}
